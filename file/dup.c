f1f0d5c8e5b0262de6cec501e8290aaccce949ec
liwy <535767595@qq.com>
2025-02-14 18:22:35 +0800

    更新代码

--- file/dup.c
+++ file/dup.c
@@ -1,42 +1,36 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+#include "unix_file.h"
 
 int main() {
   // 打开文件
-  int fd = open("file.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  int fd = open_file2("file.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
   if (fd < 0) {
-    perror("open");
     return 1;
   }
 
   // 复制文件描述符
-  int fd_copy = dup(fd);
+  int fd_copy = dup_file(fd);
   if (fd_copy < 0) {
-    perror("dup");
     return 1;
   }
 
   // 使用fd写入文件
   const char *msg1 = "This is written by fd\n";
-  ssize_t bytes_written = write(fd, msg1, strlen(msg1));
+  ssize_t bytes_written = write_file(fd, msg1, strlen(msg1));
   if (bytes_written < 0) {
-    perror("write");
     return 1;
   }
 
   // 使用fd_copy写入文件
   const char *msg2 = "This is written by fd_copy\n";
-  bytes_written = write(fd_copy, msg2, strlen(msg2));
+  bytes_written = write_file(fd_copy, msg2, strlen(msg2));
   if (bytes_written < 0) {
-    perror("write");
+
     return 1;
   }
 
   // 关闭文件描述符
-  close(fd);
-  close(fd_copy);
+  close_file(fd);
+  close_file(fd_copy);
 
   return 0;
 }
--- file/dup2.c
+++ file/dup2.c
@@ -1,21 +1,15 @@
-#include <apue.h>
-#include <fcntl.h>
+#include "unix_file.h"
 
 int main() {
-
-  int fd = open("file-dup2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  int fd = open_file2("file-dup2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
   if (fd < 0) {
-    err_sys("open error");
     return 1;
   }
-
-  int copy_fd2 = dup2(fd, STDOUT_FILENO);
+  int copy_fd2 = dup2_file(fd, STDOUT_FILENO);
   if (copy_fd2 < 0) {
-    err_sys("dup2 error");
     return 1;
   }
-
   printf("This will be written to file\n");
-  close(fd);
+  close_file(fd);
   return 0;
 }
