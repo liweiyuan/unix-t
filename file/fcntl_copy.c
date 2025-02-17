8c259242e19dc7c34602944a319400780751fe62
liwy <535767595@qq.com>
2025-02-17 15:07:46 +0800

    更新原始的操作

--- file/fcntl_copy.c
+++ file/fcntl_copy.c
@@ -1,8 +1,7 @@
-#include <apue.h>
-#include <fcntl.h>
+#include "unix_file.h"
 
 int main() {
-  int fd = open("copy.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  int fd = open_file2("copy.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
   if (fd < 0) {
     err_sys("open error");
     return 1;
@@ -16,10 +15,10 @@ int main() {
   }
 
   // 通过2个文件描述符写入数据
-  write(fd, "Hello, ", 7);
-  write(newfd, "World!\n", 7);
+  write_file(fd, "Hello, ", 7);
+  write_file(newfd, "World!\n", 7);
 
-  close(fd);
-  close(newfd);
+  close_file(fd);
+  close_file(newfd);
   return 0;
 }
